01a4fbc77315d104526dff40f4141f1cc12071ab
jaeho kang <greennuri@gmail.com>
2024-06-27 02:22:20 +0900

    view 수정 및 html 분할

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+# from django.shortcuts import render
+from django.template import loader
+import datetime
 # Create your views here.
 def index(request):
-    return render(request, 'base.html')
+    template = loader.get_template('main.html')
+    now = datetime.datetime.now()   
+    context = {"current_date": now, "name": "jhkang"}
+    return HttpResponse(template.render(context, request))
+    # return render(request, 'main.html')
